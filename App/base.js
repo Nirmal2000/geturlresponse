aec007edb568985a44cda9a330c2f0543478e197
Nirmal2000 <44404757+Nirmal2000@users.noreply.github.com>
2019-06-17 11:41:35 +0530

    Delete base.js

--- App/base.js
+++
@@ -1,9 +0,0 @@
-var url = require('url')
-
-exports.render_html = function(){
-    var g = url.parse(req.url).query
-                arr= g.split('=')
-                data = arr[1]
-                return console.log(data)
-    
-}
