d5cfff8bacfb24afa5fe784c15ff47be4d1be69d
Nirmal <nirmalvelu2000@gmail.com>
2019-07-02 09:33:09 +0530

    Waits for internet, returns response once online

--- cachereq.js
+++ cachereq.js
@@ -17,8 +17,19 @@ self.addEventListener('fetch',e=>{
             .then(cache=>{
                 cache.put(e.request,resClone)
             })
+            console.log("S")
             return res
         })
-        .catch(()=> caches.match(e.request).then(res=>res))        
-    )
+        .catch(()=> caches.match(e.request).then(res=>{if(res){return res}else{throw "E"}}).catch(()=>{
+
+console.log("NOT IN CACHE")
+console.log(navigator.onLine)
+ clients.get(e.clientId).then(client=>{client.postMessage({
+      msg: "Hey I just got a fetch from you!",
+      url: e.request.url
+    });
+    })
+
+}))
+)
 })
